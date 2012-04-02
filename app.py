10ced9e9eb1605d84c255184ca0225cf83903f74
Sergey Parshukov <sergey.parshukov@gizbo.ru>
2012-04-02 13:52:51 +0400

    Fixed test for linux

--- app.py
+++ app.py
@@ -7,17 +7,16 @@ from lowlevel import LowLevelApi
 #from game import GameApi
 from point import Point
 from constants import *
-from mac import Image
 
 ## Tests ##
 if __name__ == '__main__':
-	
+
 	# Drag HP pot to slot 7
 	api = LowLevelApi()
-	hp = api.loadIcon('assets/hp.png').hash()
+	hp = api.loadIcon('assets/hp.png')
 	for slot in range(1, 9):
-		icon = api.getSlotImage(slot).hash()
-		isHp = icon == hp
+		icon = api.getSlotImage(slot)
+		isHp = icon.isEqual(hp)
 		print 'Slot {0} HP={1}'.format(slot, isHp)
 		if isHp:
 			api.dragSlotToSlot(slot, 7)
--- linux.py
+++ linux.py
@@ -10,14 +10,14 @@ from constants import *
 
 class Image:
 	data = None
-	
+
 	def __init__(self, src):
 		self.data = src
-	
-	def isEqual(self, image):
+
+	def isEqual(self, image, size = (32, 32)):
 		for x in range(0, size[0] - 1):
 			for y in range(0, size[1] - 1):
-				if self.data.get_pixel(x, y) != image.get_pixel(x, y):
+				if self.data.get_pixel(x, y) != image.data.get_pixel(x, y):
 					return False
 		return True
 
@@ -30,18 +30,18 @@ class PlatformSpecificApi:
 		self.window = gtk.gdk.window_foreign_new(int(self.windowId))
 		if not self.window:
 			raise WindowException('Can not get GTK window')
-	
+
 	def loadIcon(self, filename):
 		pixbuf = gtk.gdk.pixbuf_new_from_file(filename)
 		pixmap = gtk.gdk.Pixmap(self.window, 32, 32, -1)
 		pixmap.draw_pixbuf(None, pixbuf, 0, 0, 0, 0, -1, -1)
 		return Image(pixmap.get_image(0, 0, 32, 32))
-	
+
 	def getImageAtPoint(self, point, size = Point(32, 32)):
 		point = point + self.getLocalOffset()
 		img = self.window.get_image(point.x + 1, point.y, size.x, size.y)
 		return Image(img)
-	
+
 	def getVerticalWindowOffset(self):
 		x = self.window.get_geometry()[2] / 2
 		for y in range(0, 200):
@@ -49,22 +49,22 @@ class PlatformSpecificApi:
 			if(img.get_pixel(0, 0) == 0x000 and img.get_pixel(0, 1) == 0x000 and img.get_pixel(0, 2) == 0x000):
 				return y
 		return 0
-	
+
 	def getLocalOffset(self):
 		return Point(self.window.get_geometry()[2] / 2 - GAME_SIZE.x / 2, self.verticalWindowOffset)
-	
+
 	def getOffset(self):
 		origin = self.window.get_origin()
 		localOffset = self.getLocalOffset()
 		return Point(origin[0] + localOffset.x, origin[1] + localOffset.y)
-	
+
 	def click(self, point):
 		point = point + self.getLocalOffset()
 		subprocess.call(['xdotool', 'windowactivate', '--sync', self.windowId,
 										 'mousemove', '--sync', str(point.x), str(point.y),
 										 'click', '1',
 										 'mousemove', 'restore'])
-	
+
 	def drag(self, pointA, pointB):
 		offset = self.getOffset()
 		pointA = pointA + offset
@@ -75,11 +75,11 @@ class PlatformSpecificApi:
 										 'mousemove', '--sync', str(pointB.x), str(pointB.y),
 										 'mouseup', '1',
 										 'mousemove', 'restore'])
-	
+
 	def testPixel(self, point, colorCode):
 		point = point + self.getLocalOffset()
 		pixel = self.window.get_image(point.x, point.y, 1, 1).get_pixel(0, 0)
 		return pixel == colorCode
-	
+
 	def sendkey(self, key):
 		subprocess.call(['xdotool', 'windowactivate', '--sync', self.windowId, 'key', key])
